73249ff3a9c63fd27ef01f571db27ee5c872652a
mxt299 <mohan.thirunavukkarasu@sherwin.com>
2014-10-29 13:05:49 -0400

    implemented StoreMulitpleNumbersMemory

---
+++ Calculator/StoreListOfNumbersMemoryTests.cs
@@ -0,0 +1,38 @@
+﻿using CalculatorLib;
+using NUnit.Framework;
+
+namespace CalculatorTest
+{
+    [TestFixture]
+    public class StoreListOfNumbersMemoryTests
+    {
+         [Test]
+         public void StoreListOfNumbers_MemPlus_StoresTheList()
+         {
+             var expectedNumber = 10;
+             var memory = new StoreMulitpleNumbersMemory();
+             memory.MemPlus(expectedNumber);
+
+             var result = memory.MemRetrieve();
+
+             Assert.That(result, Is.EqualTo(expectedNumber));
+         }
+
+         [Test]
+         public void StoreListOfNumbers_MemRetrieve_RetrievesTheList_InReverseOrder()
+         {
+             var firstNumber = 10;
+             var secondNumber = 20;
+             var memory = new StoreMulitpleNumbersMemory();
+             memory.MemPlus(firstNumber);
+             memory.MemPlus(secondNumber);
+
+             var result = memory.MemRetrieve();
+
+             Assert.That(result, Is.EqualTo(secondNumber));
+
+             result = memory.MemRetrieve();
+             Assert.That(result, Is.EqualTo(firstNumber));
+         }
+    }
+}
---
+++ CalculatorLib/StoreMulitpleNumbersMemory.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using CalculatorTest;
+
+namespace CalculatorLib
+{
+    public class StoreMulitpleNumbersMemory : ICalculatorMemory
+    {
+        Stack<int> _memory = new Stack<int>();
+        public void MemPlus(int number)
+        {
+            _memory.Push(number);
+        }
+
+        public int MemRetrieve()
+        {
+            return _memory.Pop();
+        }
+    }
+}
